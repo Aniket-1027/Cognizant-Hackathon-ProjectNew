7cef99665032d7e7b195e1040d3618bbb53d1696
Aniket <ak2710aniketkumar@gmail.com>
2026-04-29 17:17:43 +0530

    Included Homepage code

--- src/main/java/org/zigwheels/pages/HomePage.java
+++ src/main/java/org/zigwheels/pages/HomePage.java
@@ -1,47 +1,188 @@
 package org.zigwheels.pages;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class HomePage extends CommonCode {
+public class HomePage{
+    WebDriver driver;
+    JavascriptExecutor js;
+
     public HomePage(WebDriver driver){
-
-        super(driver);
+        this.driver = driver;
+        this.js = (JavascriptExecutor) driver;
+        PageFactory.initElements(driver, this);
     }
 
-    @FindBy(xpath = "(//span[@class='c-p ml-5'])[3]")
-    WebElement newBikes;
+    @FindBy(xpath="//div[@class='e1e158e66b afdff78c97 ce103e449e fe169dc72b']")
+    WebElement closePopupBtn;
 
-    @FindBy(xpath = "(//a[text()='Upcoming Bikes'])[1]")
-    WebElement upcmngBikes;
+    @FindBy(xpath="//input[@placeholder='Where are you going?']")
+    WebElement searchCity;
 
-    @FindBy(xpath = "//span[@class='c-p']")
-    WebElement more;
+    @FindBy(xpath="//button[@data-testid='searchbox-dates-container']")
+    WebElement clickDateElement;
 
-    @FindBy(xpath = "//a[text()='Used Cars']")
-    WebElement usedCars;
+    @FindBy(xpath="(//div[@data-testid = 'searchbox-datepicker-calendar']//following::h3)[1]")
+    WebElement currentMonthAndYear;
 
-    @FindBy(xpath = "//div[@id='des_lIcon']")
-    WebElement login;
+    @FindBy(xpath="//span[@class='ecb788f3b7 c0b8f1e8f8 d9382a910a']//span")
+    WebElement currentDay;
 
-    public void clickNewBikes(){
-        super.clickByJS(newBikes);
+    @FindBy(xpath="//button[@aria-label='Next month']")
+    WebElement nextMonthBtn;
+
+    @FindBy(xpath="//button[@data-testid='occupancy-config']")
+    WebElement guestSelector;
+
+    @FindBy(xpath="(//span[@class='e32aa465fd'])[1]")
+    WebElement checkAdultCount;
+
+    @FindBy(xpath="(//button[@tabindex = '-1'])[2]")
+    WebElement adultPlusBtn;
+
+    @FindBy(xpath="(//div[@class='e301a14002']//button)[1]")
+    WebElement adultMinusBtn;
+
+    @FindBy(xpath="//span[normalize-space()='Done']")
+    WebElement applyBtn;
+
+    @FindBy(xpath="//span[text()='Search']")
+    WebElement searchBtn;
+
+    public void closePop(){
+        try{
+            closePopupBtn.click();
+        }catch(ElementClickInterceptedException e){
+            js.executeScript("arguments[0].click()", closePopupBtn);
+        }
     }
 
-    public void clickUpcmngBikes(){
-        super.clickByJS(upcmngBikes);
+    public void searchCity(String enterCity){
+        searchCity.sendKeys(enterCity);
+        searchCity.sendKeys(Keys.TAB);
     }
 
-    public void clickMore(){
-        super.clickByJS(more);
+    public void startDate(String startDay, String startMonth, String startYear){
+        try{
+            clickDateElement.click();
+        }catch(ElementClickInterceptedException e){
+            js.executeScript("arguments[0].click()", clickDateElement);
+        }
+        String[] currentMonthYear = currentMonthAndYear.getText().split(" ");
+        String currentMonth = "";
+        String currentYear = "";
+        try{
+            currentMonth = currentMonthYear[0];
+            currentYear = currentMonthYear[1];
+        }catch(IndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+            return;
+        }
+        String currentDayStr = currentDay.getText();
+        int currentDayInt = Integer.parseInt(currentDayStr);
+        int startDayInt = Integer.parseInt(startDay);
+        while(!currentYear.equals(startYear)){
+            try{
+                nextMonthBtn.click();
+            }catch(ElementClickInterceptedException e){
+                js.executeScript("arguments[0].click()", nextMonthBtn);
+            }
+            currentMonthYear = currentMonthAndYear.getText().split(" ");
+            currentMonth = currentMonthYear[0];
+            currentYear = currentMonthYear[1];
+        }
+        while(!currentMonth.equals(startMonth)){
+            try{
+                nextMonthBtn.click();
+            }catch(ElementClickInterceptedException e){
+                js.executeScript("arguments[0].click()", nextMonthBtn);
+            }
+            currentMonthYear = currentMonthAndYear.getText().split(" ");
+            currentMonth = currentMonthYear[0];
+        }
+        if(startDayInt >= currentDayInt){
+            driver.findElement(By.xpath("(//span[text()='"+startDay+"'])[1]")).click();
+        } else {
+            System.out.println("Expected day is before current day, cannot select.");
+        }
     }
 
-    public void clickUsedCars(){
-        super.clickByJS(usedCars);
+    public void endDate(String endDay, String endMonth, String endYear){
+        String[] currentMonthYear = currentMonthAndYear.getText().split(" ");
+        String currentMonth = "";
+        String currentYear = "";
+        try{
+            currentMonth = currentMonthYear[0];
+            currentYear = currentMonthYear[1];
+        }catch(IndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+            return;
+        }
+        while(!currentYear.equals(endYear)){
+            try{
+                nextMonthBtn.click();
+            }catch(ElementClickInterceptedException e){
+                js.executeScript("arguments[0].click()", nextMonthBtn);
+            }
+            currentMonthYear = currentMonthAndYear.getText().split(" ");
+            currentMonth = currentMonthYear[0];
+            currentYear = currentMonthYear[1];
+        }
+        while(!currentMonth.equals(endMonth)){
+            try{
+                nextMonthBtn.click();
+            }catch(ElementClickInterceptedException e){
+                js.executeScript("arguments[0].click()", nextMonthBtn);
+            }
+            currentMonthYear = currentMonthAndYear.getText().split(" ");
+            currentMonth = currentMonthYear[0];
+        }
+
+        driver.findElement(By.xpath("(//span[text()='"+endDay+"'])[1]")).click();
+        clickDateElement.sendKeys(Keys.TAB);
     }
 
-    public  void clickLogin(){
-        super.clickByJS(login);
+    public void enterNumberOfAdults(int targetAdults){
+        try{
+            guestSelector.click();
+        }catch(ElementClickInterceptedException e){
+            js.executeScript("arguments[0].click()", guestSelector);
+        }
+        int currentAdultsSelected = Integer.parseInt(checkAdultCount.getText());
+        while(currentAdultsSelected < targetAdults){
+            try{
+                adultPlusBtn.click();
+            }catch(ElementClickInterceptedException e){
+                js.executeScript("arguments[0].click()", adultPlusBtn);
+            }
+            currentAdultsSelected = Integer.parseInt(checkAdultCount.getText());
+        }
+        while(currentAdultsSelected > targetAdults){
+            try{
+                adultMinusBtn.click();
+            }catch(ElementClickInterceptedException e){
+                js.executeScript("arguments[0].click()", adultMinusBtn);
+            }
+            currentAdultsSelected = Integer.parseInt(checkAdultCount.getText());
+        }
+        try{
+            applyBtn.click();
+        }catch(ElementClickInterceptedException e){
+            js.executeScript("arguments[0].click()", applyBtn);
+        }
+
     }
+
+    public void search(){
+        try{
+            searchBtn.click();
+        }catch(ElementClickInterceptedException e){
+            js.executeScript("arguments[0].click()", searchBtn);
+        }
+    }
+
+
+
+
 }
--- src/test/java/basetest/BaseTest.java
+++ src/test/java/basetest/BaseTest.java
@@ -1,52 +1,40 @@
 package basetest;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.io.FileHandler;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.*;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utilities.Utility;
 import java.time.Duration;
-import java.util.Properties;
 
 public class BaseTest {
-     protected static WebDriver driver;
-     protected static WebDriverWait wait;
-     public Logger logger;
-     public Properties p;
 
-     @BeforeClass
-     public void setDriver() throws IOException {
-         ChromeOptions options = new ChromeOptions();
-         options.addArguments("--disable-notifications");
-         options.addArguments("--headless=new");
-         driver = new ChromeDriver(options);
-         FileReader file=new FileReader(".//src//test//resources//config.properties");
-         p=new Properties();
-         p.load(file);
-         logger = LogManager.getLogger(this.getClass());
-         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-         driver.get("https://www.zigwheels.com/");
-         driver.manage().window().maximize();
-         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-     }
+    public WebDriver driver;
 
-     @AfterClass
-     public void tearDown() {
-         driver.quit();
-     }
+    @BeforeMethod
+    public void setUp() throws Exception {
+        if(Utility.fetchPropertyValue("browser").equals("chrome")) {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }else if(Utility.fetchPropertyValue("browser").equals("firefox")){
+            driver = new FirefoxDriver();
+            driver.manage().window().maximize();
+        }
+        else if(Utility.fetchPropertyValue("browser").equals("edge")){
+            driver = new EdgeDriver();
+            driver.manage().window().maximize();
+        }
+        driver.get(Utility.fetchPropertyValue("baseUrl").toString());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Long.parseLong(Utility.fetchPropertyValue("implicitWaitSeconds").toString())));
+    }
 
-     public static void takeScreenShot(WebDriver driver, String fileName) throws IOException {
-         File screenshotsDir = new File(System.getProperty("user.dir") + "/screenshots");
-         TakesScreenshot ts = (TakesScreenshot) driver;
-         File src = ts.getScreenshotAs(OutputType.FILE);
-         File destination = new File(screenshotsDir, fileName + ".png");
-         FileHandler.copy(src, destination);
-     }
- }
+//    @AfterMethod
+//    public void tearDown() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//    }
+}
+
