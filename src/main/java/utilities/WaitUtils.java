85c89d253f5cd878bd95807250a4a7c6b806db5d
Slipstream <hatkardhruv@gmail.com>
2026-04-29 21:45:45 +0530

    feat: added ScreenshotUtil and WaitUtils

---
+++ src/main/java/utilities/WaitUtils.java
@@ -0,0 +1,12 @@
+package utilities;
+
+public class WaitUtils {
+    
+    public static void sleep(long milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
