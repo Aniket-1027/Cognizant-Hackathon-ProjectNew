cd6a073d64364d8f8ba6b99b564a3b24544a4937
Soumik <csoumik10@gmail.com>
2026-05-04 12:35:50 +0530

    Included a method

--- src/main/java/org/booking/pages/HotelSearchPage.java
+++ src/main/java/org/booking/pages/HotelSearchPage.java
@@ -39,7 +39,7 @@ public class HotelSearchPage {
     WebElement findPropertiesBtn;
 
     @FindBy(xpath="//div[contains(text(),'Hotels') and @data-testid='filters-group-label-content']")
-    WebElement checkHotelsOption;
+    public WebElement checkHotelsOption;
 
     @FindBy(xpath = "//div[contains(text(),'Wonderful') and @data-testid='filters-group-label-content']")
     WebElement checkWonderfulOption;
@@ -157,5 +157,10 @@ public class HotelSearchPage {
 
     public List <WebElement> getReviewLabels() {
         return reviewLabels;
+    }
+
+    public String getSmartFilterText(){
+        String value = smartFilterTextArea.getAttribute("value");
+        return value;
     }
 }
